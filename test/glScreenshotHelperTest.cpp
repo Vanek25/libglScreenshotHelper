98eae8785204550dd45e11a8c87df971308cc9b2
Ivan <kostenko98i@mail.ru>
2023-04-27 14:18:56 +0300

    + начал писать тесты

---
+++ test/glScreenshotHelperTest.cpp
@@ -0,0 +1,12 @@
+#include <cppunit/config/SourcePrefix.h>
+#include "../include/glScreenshotHelper.h"
+#include "glScreenshotHelperTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScreenshotHelperTest);
+
+void ScreenshotHelperTest::testCreateFilename()
+{
+    std::cout<<"\nSetup()";
+    ScreenshotHelper shelper;
+
+}
