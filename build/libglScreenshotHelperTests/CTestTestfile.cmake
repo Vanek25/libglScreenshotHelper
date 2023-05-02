a897fc7103ce1e2b6407369f722d55823e5e89b9
Ivan <ksotenko98i@mail.ru>
2023-05-02 16:54:32 +0300

    без папки build

--- build/libglScreenshotHelperTests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neon/projects/tals/libglScreenshotHelperTests
-# Build directory: /home/neon/projects/tals/build/libglScreenshotHelperTests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/home/neon/projects/tals/build/libglScreenshotHelperTests/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/home/neon/projects/tals/libglScreenshotHelperTests/CMakeLists.txt;16;add_test;/home/neon/projects/tals/libglScreenshotHelperTests/CMakeLists.txt;0;")
