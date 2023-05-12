26ea2f88b52dab7c6a64eb7f4d710cde5d7a741d
Ivan <ksotenko98i@mail.ru>
2023-05-12 17:12:54 +0300

    ^ подправил размер переменной pathToLoadScreenshot [... +1], теперь ошибки perv.size() != cur.size() нет

---
+++ build/CMakeFiles/glScreenshotHelper_static.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "libglScreenshotHelper_static.a"
+  "libglScreenshotHelper_static.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/glScreenshotHelper_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- include/glScreenshotHelper.h
+++ include/glScreenshotHelper.h
@@ -18,6 +18,4 @@ namespace vniiftri
     }
 }
 
-
-
 #endif
