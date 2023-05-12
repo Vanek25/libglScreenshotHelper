94f3337518caff2e9c9d9c0737819505609d66f2
Ivan <ksotenko98i@mail.ru>
2023-05-12 17:15:39 +0300

    - папка build

--- build/CMakeFiles/glScreenshotHelper_static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libglScreenshotHelper_static.a"
-  "libglScreenshotHelper_static.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/glScreenshotHelper_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
